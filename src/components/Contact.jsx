5ecbf4099effa5695b5efe183665670bf4215e4f
Monicah Cloud <monicahcloud@gmail.com>
2024-04-08 17:59:20 -0500

    contact page updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 
 const Contact = () => {
+ const info = {
+    name:"Nerah's Passion",
+    address: "Serving the following areas: Milwaukee, Wauwatosa, Brookfield, West Allies",
+    email: "nerahspassion@yahoo.com",
+    phone: "414-522-1811",
+  };
   return (
-    <div>
-      <h1>Contact Page</h1>
-    </div>
+    <>
+      <Container>
+      <Row>
+        <h1  style={{
+              color: "white",
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+              padding: 15,
+            }}>Contact Us</h1>
+        <Col
+         style={{
+          color: "white",
+          // display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 15,
+        }}>
+          <h3>
+        <div>{info.name}</div>
+        <div>Address: {info.address}</div>
+        <div>Phone: {info.phone}</div>
+        <div>Email: {info.email}</div>
+       </h3>
+        </Col>
+        <Col>
+        <Image
+            className=''
+              rounded
+              width="200"
+              height="175"
+              src="https://api.dicebear.com/7.x/adventurer-neutral/svg?seed=mail@ashallendesign.co.uk" alt="new clients" />
+        </Col>
+      </Row>
+      </Container>
+    </>
   )
 }
 
