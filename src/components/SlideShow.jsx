4ee3d7100ecdcc6af2f02049560b996389a41f35
Monicah Cloud <monicahcloud@gmail.com>
2024-04-08 16:15:21 -0500

    slideshow working just need to add pictures

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
-import Row from 'react-bootstrap/Row';
+import '../App.css';
 
 const SlideShow = () => {
   // const images = [
@@ -21,41 +18,34 @@ const SlideShow = () => {
   return (
     <>
     <div className='SlideShowbody'  style={{ border:'10px solid rgba(255, 240, 0)'}}>
-    {/* <Container>
-      <Row>
-        <Col xs={3}>
-          {images.map(image=> {
-            return (
 
-              <Image style={{ width: 150, height: 150 }} src={image.url} roundedCircle/>
-            )
-          })}
-          </Col>
-      </Row>
-    </Container> */}
+<div className='images'>
+  <div className='images-slide'>
+  <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/09/18/16841819_d48e7e9e_720x540.jpg" alt="  locks"  />
+        
+  <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/12/11/17120033_29c7b5c6_720x540.jpg"  alt="  locks2 " />
+        
+  <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/09/18/16841812_a30567fa_720x540.jpg"alt="  locks3"  />
+        
+  <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2024/02/15/17339046_9fdc8a95_720x540.jpg" alt="  locks4" />
+        
+  <img src="https://dummyimage.com/16:9x1080" alt="  locks5" />
+        
+  </div>
+  <div className='images-slide'>
+  <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/09/18/16841819_d48e7e9e_720x540.jpg" alt="  locks6"  />
+        
+  <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/12/11/17120033_29c7b5c6_720x540.jpg" alt="  locks7" />
+        
+  <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/09/18/16841812_a30567fa_720x540.jpg" alt="  locks8" />
+        
+  <img src="https://dummyimage.com/16:9x1080" alt="  locks9" />
+        
+  <img src="https://dummyimage.com/16:9x1080"  alt="  locks10"/>
+        
+  </div>
 
-<Container style={{ margin:"0px", padding:"10px"}}>
-      <Row  >
-           <Col xs={2} >
-          <Image style={{ width: 150, height: 150 }} src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/10/18/16946850_65be2e6f_720x540.jpg" roundedCircle />
-        </Col>
-        <Col xs={2} >
-          <Image style={{ width: 150, height: 150 }}src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/12/11/17120033_29c7b5c6_720x540.jpg" roundedCircle />
-        </Col>
-        <Col xs={2} >
-          <Image style={{ width: 150, height: 150 }}src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/09/18/16841819_d48e7e9e_720x540.jpg" roundedCircle />
-        </Col>
-        <Col xs={2} >
-          <Image style={{ width: 150, height: 150 }}src="https://dummyimage.com/16:9x1080" roundedCircle />
-        </Col>
-        <Col xs={2} >
-          <Image style={{ width: 150, height: 150 }}src="https://dummyimage.com/16:9x1080" roundedCircle />
-        </Col>
-        <Col xs={2} >
-          <Image style={{ width: 150, height: 150 }}src="https://dummyimage.com/16:9x1080" roundedCircle />
-        </Col>
-        </Row>
-    </Container>
+</div>
 
     </div>
     </>
