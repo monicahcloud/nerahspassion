1f40a38ff917b2f7d711ad018bfb16f7390e67ca
Monicah Cloud <monicahcloud@gmail.com>
2024-04-08 17:27:53 -0500

    services page updated

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,72 +2,77 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-// import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
 import '../App.css';
-// import transfer from "../images/newclients.png"
+import transfer from "../images/newclients.png"
 
 const Services = () => {
 
   const services = [
-    { service: "Sisterlocks®/ Brotherlocks", key: 0 },
-    { service: "Interlocks", key: 1 },
+    { service: "Sisterlocks®/ Brotherlocks (Consultation is required)", key: 0 },
+    { service: "Microlocs (Consultation is required)", key: 1 },
     { service: "Lock Repair", key: 2 },
-    { service: "Traditional Locks", key: 3 }
+    { service: "Traditional Locks", key: 3 },
+    { service: "Retigthening/Interlocking", key: 4 },
+    { service: "Sisterlocks® Establishment/ Microloc Establishment", key: 5 },
+    { service: "Transfer Client Fees", key: 6 },
+    { service: "Lock Maintance", key: 7 },
   ];
 
   return (
     <>
-      <Container classname="  " style={{
-              color: "white",
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'center',
-              padding:50
-            }}>
-
+      <Container classname="  " >
         <Row className='playfair-display'>
-          <Col md={12}
+          <Col md={8}
             style={{
               color: "white",
-              display: 'flex',
+              // display: 'flex',
               flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'center',
+              alignItems: 'left',
+              justifyContent: 'left',
+              padding: 15,
             }}>
-            
             <ul>
               <h2 style={{
-              color: "white",
-              textDecoration:"underline",
-            }}> Services</h2>
+                color: "white",
+                textDecoration: "underline",
+              }}> Services</h2>
               {services.map(service => {
                 return (
-                 <h4> 
-                  <li key={service.key}
-                  >{service.service}
-                  </li>
+                  <h4>
+                    <li key={service.key}
+                    >{service.service}
+                    </li>
                   </h4>
                 )
               })}
-
             </ul>
+            <Button
+              variant="warning"
+              size="lg"
+              href='https://www.styleseat.com/m/v/nerahferguson?fbclid=IwAR0J9PA3UKHeTK28pRnrL12SeGqtkMoILxaLMO-wzy3P-EsF9lgoiowhFkc_aem_AZxJkILJV-oKLYqKcEy8Abf7GAZOpyTRAorHdrcmZ1YYw1xqpUUTWtBTDJDrHYTUwbSrdP4Jn8sQ9R9wUsWORdyp'
+              target="_blank"
+              active>
+              Book Appointment
+            </Button>
           </Col>
-          {/* <Col md={4}  
-          style={{
-              display: 'flex',
+          <Col md={4}
+            style={{
+              // display: 'flex',
               flexDirection: 'row',
-              alignItems: 'right',
-              justifyContent: 'right',
-              marginTop:30
-
-            }}>
-              <Image 
-            rounded
-            width="200"
-            height="175"
-            src={transfer} alt="new clients" />
-          </Col> */}
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginTop: 30
+            }}
+          >
+            <Image
+              class
+              rounded
+              width="200"
+              height="175"
+              src={transfer} alt="new clients" />
+          </Col>
         </Row>
       </Container>
     </>
--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -8,17 +8,29 @@ const SlideShow = () => {
       <div className='SlideShowbody' style={{ border: '10px solid rgba(255, 240, 0)' }}>
         <div className='images'>
           <div className='images-slide'>
-          <img src="https://dummyimage.com/16:9x1080" alt="  locks6" />
+          <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2024/04/08/17518745_771f593e_720x540.jpg" alt="  locks6" />
             <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/12/11/17120033_29c7b5c6_720x540.jpg" alt="  locks7" />
             <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/09/18/16841812_a30567fa_720x540.jpg" alt="  locks8" />
-            <img src="https://dummyimage.com/16:9x1080" alt="  locks9" />
+            <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2024/04/08/17518738_0c8481e7_720x540.jpg" alt="  locks9" />
+            <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/09/18/16841819_d48e7e9e_720x540.jpg" alt="  locks10" />
+            <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/12/05/17099261_5b9b3ce0_720x540.jpg" alt="  locks6" />
+            <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/11/29/17081627_1f60fdfa_720x540.jpg" alt="  locks7" />
+            <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/09/18/16841812_a30567fa_720x540.jpg" alt="  locks8" />
+            <img src="https://scontent-ord5-1.xx.fbcdn.net/v/t39.30808-6/430613534_10225738166381146_8238702693416104038_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-oOrSvgQTnQAb5vUIl-&_nc_ht=scontent-ord5-1.xx&oh=00_AfC7PpEO6M9wDufOp11Nri0dbnuDIi-yK-Bzwbpm2oY0Aw&oe=661A240A" alt="  locks9" />
             <img src="https://dummyimage.com/16:9x1080" alt="  locks10" />
-            <img src="https://dummyimage.com/16:9x1080" alt="  locks6" />
+          </div> 
+          <div className='images-slide'>
+          <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2024/04/08/17518745_771f593e_720x540.jpg" alt="  locks6" />
             <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/12/11/17120033_29c7b5c6_720x540.jpg" alt="  locks7" />
             <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/09/18/16841812_a30567fa_720x540.jpg" alt="  locks8" />
-            <img src="https://dummyimage.com/16:9x1080" alt="  locks9" />
+            <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2024/04/08/17518738_0c8481e7_720x540.jpg" alt="  locks9" />
+            <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/09/18/16841819_d48e7e9e_720x540.jpg" alt="  locks10" />
+            <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/12/05/17099261_5b9b3ce0_720x540.jpg" alt="  locks6" />
+            <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/11/29/17081627_1f60fdfa_720x540.jpg" alt="  locks7" />
+            <img src="https://d220aniogakg8b.cloudfront.net/static/uploads/2023/09/18/16841812_a30567fa_720x540.jpg" alt="  locks8" />
+            <img src="https://scontent-ord5-1.xx.fbcdn.net/v/t39.30808-6/430613534_10225738166381146_8238702693416104038_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-oOrSvgQTnQAb5vUIl-&_nc_ht=scontent-ord5-1.xx&oh=00_AfC7PpEO6M9wDufOp11Nri0dbnuDIi-yK-Bzwbpm2oY0Aw&oe=661A240A" alt="  locks9" />
             <img src="https://dummyimage.com/16:9x1080" alt="  locks10" />
-          </div>         
+          </div>            
         </div>
       </div>
     </>
